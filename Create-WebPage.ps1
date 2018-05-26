9fcdea35144a946427eef3dcf9ba325c5cc4c232
raph <raphael@haddad.id.au>
2018-05-26 11:07:13 +1000

    Parse file with proper HTML

--- Create-WebPage.ps1
+++ Create-WebPage.ps1
@@ -1,12 +1,9 @@
 $html = (Get-Content .\README.md) `
-                -replace '^## (.+)', '<h2>$1</h2>'
-
-$html = $html -replace '^# (.+)', '<h1>$1</h1>'
-
-$html = $html -replace '    (.+)', '<input type="text" class="code stretch" value="$1" ><button class="copy-button stretch">Copy to clipboard</button>'
-
-$html = $html -replace '<h2>', '<div><h2>'
-$html = $html -replace '<h2>', '<div><h2>'
-$html = $html -replace '</button>', '</div>'
+                -replace '^## (.+)', '<h2>$1</h2>' `
+                -replace '^# (.+)', '<h1>$1</h1>' `
+                -replace '    (.+)', '<input type="text" class="code stretch" value="$1" ><button class="copy-button stretch">Copy to clipboard</button>' `
+                -replace '<h2>', '<div><h2>' `
+                -replace '<h2>', '<div><h2>' `
+                -replace '</button>', '</div>'
 
 Write-Host "$html"
