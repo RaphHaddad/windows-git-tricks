7c5fcf1096eb532c368b132c8e20b914ef33c793
raph <raphael@haddad.id.au>
2018-05-26 10:20:02 +1000

    Before regex

--- Create-WebPage.ps1
+++ Create-WebPage.ps1
@@ -3,14 +3,15 @@ $lines = Get-Content .\README.md
 
 $bodyHtml = ""
 $hasStartedNewSnipet = $false
+$hasStartedNewCodeBlock = $false
 foreach ($line in $lines) {
     if ($line.StartsWith('## ')) {
         $hasStartedNewSnipet = $true
         $line = $line.Trim('## ')
         $bodyHtml += "<div><h2>$line</h2>"
-    }
+    } 
 
-    if (-not $line -and $hasStartedNewSnipet) {
+    if (-not $line -and $hasStartedNewSnipet -and -not $hasStartedNewCodeBlock) {
         $bodyHtml += "</div>"
         $hasStartedNewSnipet = $false
     }
