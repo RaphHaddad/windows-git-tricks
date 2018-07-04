69ce2aa643d1914306bd7e29edf3a18193c75a20
raph <raphael@haddad.id.au>
2018-07-04 15:58:59 +1000

    Add links to and from github and web version

--- Create-WebPage.ps1
+++ Create-WebPage.ps1
@@ -1,11 +1,13 @@
 $html = (Get-Content .\README.md) `
+                -replace '^\[Website version\] with.+$', '<p><a href=\"https://github.com/RaphHaddad/windows-git-tricks\" target=\"_blank\">GitHub source</a></p>' `
                 -replace '^## (.+)', '<h2>$1</h2>' `
                 -replace '^# (.+)', '<h1>$1</h1>' `
                 -replace '    (.+)', '<input type="text" class="code stretch" value="$1" ><button class="copy-button stretch">Copy to clipboard</button>' `
                 -replace '<h2>', '<div><h2>' `
                 -replace '<h2>', '<div><h2>' `
-                -replace '</button>', '</div>'
+                -replace '</button>', '</div>' `
+                -replace '^_(.+)_$', ''
+
 
 $page = (Get-Content .\page-template.html).Replace('{{body_html}}', $html)
-
 $page | Out-File -FilePath .\index.html
