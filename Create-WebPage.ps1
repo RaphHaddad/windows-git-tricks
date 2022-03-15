8b22698f3f4a01bfbe078d5605f4cd3536147a88
raph <raphael@haddad.id.au>
2022-03-15 14:43:11 +1100

    fix

--- Create-WebPage.ps1
+++ Create-WebPage.ps1
@@ -4,8 +4,8 @@ $html = (Get-Content .\README.md) `
                 -replace '^## (.+)', '<h2>$1</h2>' `
                 -replace '^# (.+)', '<h1>$1</h1>' `
                 -replace '    (.+)', '<input type="text" class="code stretch" value="$1" ><button class="copy-button stretch">Copy to clipboard</button>' `
-                -replace '<h2>', '<div><h2>' `
-                -replace '<h2>', '<div><h2>' `
+                -replace '<h3>', '<div><h3>' `
+                -replace '<h3>', '<div><h3>' `
                 -replace '</button>', '</div>' `
                 -replace '^_(.+)_$', ''
 
